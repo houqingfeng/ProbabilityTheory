322dae06e168014f2a3731d6a47013b95833632b
侯庆峰 <houqingfeng@le.com>
2016-05-22 19:07:39 +0800

    修改python

--- 概率论/williamSpider.py
+++ 概率论/williamSpider.py
@@ -1,20 +1,19 @@
-import sys
 import urllib2
 import re
 
-x = input("x: ")
-_500url = 'http://odds.500.com/ouzhi.php?cid=293'
+_500url = 'http://odds.sports.sina.com.cn/liveodds/'
+_oddurl = 'http://odds.jcbao.com/'
 _310url = 'http://www.310win.com/buy/jingcai.aspx?typeID=105&oddstype=2'
-pattern = re.compile(r"[0-9]\.[0-9][0-9]|[0-9][0-9]\.[0-9][0-9]")
-urlfile = urllib2.urlopen(_310url)
+pattern = re.compile(r"[1]\.[2-9][0-9]|[2-9]\.[1-9][0-9]|[0-9][1-9]\.[1-9][0-9]|[2-9]\.[0-9][0-9]|[0-9][2-9]\.[0-9][0-9]")
+urlfile = urllib2.urlopen(_500url)
 html = urlfile.read()
 urlfile.close()
 wordlist = re.findall(pattern, html)
 print wordlist
 
-fp = open("/Users/houqingfeng/Desktop/odds.txt", "w+")
-wordLocate = 4
-for i in range(0, x, 1):
+fp = open("/Users/houqingfeng/Desktop/williamOdds.txt", "w+")
+wordLocate = 0
+for i in range(0, len(wordlist), 12):
     for j in range(0, 3, 1):
         if wordLocate < len(wordlist):
             if wordLocate == len(wordlist) - 1:
@@ -24,6 +23,6 @@ for i in range(0, x, 1):
             ++wordLocate
         else:
             break
-    wordLocate = wordLocate + 3
+    wordLocate = wordLocate + 9
     fp.write('\n')
 fp.close()
